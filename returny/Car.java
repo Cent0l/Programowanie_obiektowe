d122326f61adaa0313674923357101c9002d72a4
Mateusz <115219630+Cent0l@users.noreply.github.com>
2023-11-30 15:28:17 +0100

    Update Car.java

--- returny/Car.java
+++ returny/Car.java
@@ -1,6 +1,6 @@
 public record Car(String brand,String model,double fuelConsumptionPer100km) {
     public double fuelCost(double fuelPrice,double distance)
     {
-        return ((distance)/(fuelConsumptionPer100km)*fuelPrice);
+        return ((distance/100)*(fuelConsumptionPer100km)*fuelPrice);
     }
 }
